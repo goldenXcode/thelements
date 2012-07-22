5d86d3037612f39139d2f01c11db9d382bea92c5
GurtejKanwar <GurtejKanwar@9b9bf054-3825-5e86-bee2-4475c14eea6d>
2012-07-22 05:39:12 +0000

    Fixes the IndexOutOfBoundsException in the advanced tab of Custom Elements.

--- TheElements-dev/obj/local/armeabi/objs/thelements/importgl.o.d
+++ TheElements-dev/obj/local/armeabi/objs/thelements/importgl.o.d
@@ -1,23 +1,22 @@
-/home/ryan/workspace/TheElements/obj/local/armeabi/objs/thelements/importgl.o: \
- /home/ryan/workspace/TheElements/jni/importgl.c \
- /home/ryan/workspace/TheElements/jni/importgl.h \
- /home/ryan/android-ndk-r6b/platforms/android-8/arch-arm/usr/include/GLES/gl.h \
- /home/ryan/android-ndk-r6b/platforms/android-8/arch-arm/usr/include/GLES/glplatform.h \
- /home/ryan/android-ndk-r6b/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h \
- /home/ryan/android-ndk-r6b/platforms/android-8/arch-arm/usr/include/stdint.h \
- /home/ryan/android-ndk-r6b/platforms/android-8/arch-arm/usr/include/sys/_types.h \
- /home/ryan/android-ndk-r6b/platforms/android-8/arch-arm/usr/include/machine/_types.h
+obj/local/armeabi/objs/thelements/importgl.o: jni/importgl.c \
+ jni/importgl.h \
+ /home/gurtej/android-ndk-r8/platforms/android-8/arch-arm/usr/include/GLES/gl.h \
+ /home/gurtej/android-ndk-r8/platforms/android-8/arch-arm/usr/include/GLES/glplatform.h \
+ /home/gurtej/android-ndk-r8/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h \
+ /home/gurtej/android-ndk-r8/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /home/gurtej/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /home/gurtej/android-ndk-r8/platforms/android-8/arch-arm/usr/include/machine/_types.h
 
-/home/ryan/workspace/TheElements/jni/importgl.h:
+jni/importgl.h:
 
-/home/ryan/android-ndk-r6b/platforms/android-8/arch-arm/usr/include/GLES/gl.h:
+/home/gurtej/android-ndk-r8/platforms/android-8/arch-arm/usr/include/GLES/gl.h:
 
-/home/ryan/android-ndk-r6b/platforms/android-8/arch-arm/usr/include/GLES/glplatform.h:
+/home/gurtej/android-ndk-r8/platforms/android-8/arch-arm/usr/include/GLES/glplatform.h:
 
-/home/ryan/android-ndk-r6b/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h:
+/home/gurtej/android-ndk-r8/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h:
 
-/home/ryan/android-ndk-r6b/platforms/android-8/arch-arm/usr/include/stdint.h:
+/home/gurtej/android-ndk-r8/platforms/android-8/arch-arm/usr/include/stdint.h:
 
-/home/ryan/android-ndk-r6b/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+/home/gurtej/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/_types.h:
 
-/home/ryan/android-ndk-r6b/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+/home/gurtej/android-ndk-r8/platforms/android-8/arch-arm/usr/include/machine/_types.h:
